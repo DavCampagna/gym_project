a04845f83d9c74e8a36c9d9dfe55a0368bb3870c
Davide Campagna <gdavidecampagna@gmail.com>
2019-08-25 18:52:20 +0100

    add members index

---
+++ controllers/members_controller.rb
@@ -0,0 +1,12 @@
+require("sinatra")
+require("sinatra/contrib/all")
+
+# require_relative("../models/booking.rb")
+require_relative("../models/member.rb")
+# require_relative("../models/gym_class.rb")
+also_reload("../models/*")
+
+get "/members" do
+  @members = Member.all()
+  erb(:"members/index")
+end
--- gym_app.rb
+++ gym_app.rb
@@ -2,7 +2,7 @@ require("sinatra")
 require("sinatra/contrib/all")
 # require_relative("controllers/bookings_controller")
 # require_relative("controllers/gym_classes_controller")
-# require_relative("controllers/members_controller")
+require_relative("controllers/members_controller")
 
 get "/home" do
   erb(:index)
