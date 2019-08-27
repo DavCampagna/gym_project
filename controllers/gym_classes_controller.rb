8da22838faed377d3469d03b2af0551b1d155d29
Davide Campagna <gdavidecampagna@gmail.com>
2019-08-27 12:08:05 +0100

    add require relative

--- controllers/gym_classes_controller.rb
+++ controllers/gym_classes_controller.rb
@@ -1,8 +1,8 @@
 require("sinatra")
 require("sinatra/contrib/all")
 
-# require_relative("../models/booking.rb")
-# require_relative("../models/member.rb")
+require_relative("../models/booking.rb")
+require_relative("../models/member.rb")
 require_relative("../models/gym_class.rb")
 also_reload("../models/*")
 
--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -1,9 +1,9 @@
 require("sinatra")
 require("sinatra/contrib/all")
 
-# require_relative("../models/booking.rb")
+require_relative("../models/booking.rb")
 require_relative("../models/member.rb")
-# require_relative("../models/gym_class.rb")
+require_relative("../models/gym_class.rb")
 also_reload("../models/*")
 
 get "/members" do
