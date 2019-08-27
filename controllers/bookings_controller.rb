2a0d6b97911b3a6fa47ed52ede765a673e00fbf4
Davide Campagna <gdavidecampagna@gmail.com>
2019-08-27 11:37:49 +0100

    fix add a booking

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -19,25 +19,26 @@ get "/bookings/new" do
 end
 
 post "/bookings" do
-  booking = Booking.new(params)
-  booking.save
-  erb(:"bookings/create")
+  @booking = Booking.new(params)
+  @booking.save
+  redirect to("/bookings")
+  # erb(:"bookings/create")
 end
 
-get "/bookings/:id" do
-  @booking = Booking.find(params[:id])
-  erb(:"bookings/show")
-end
-
-get "/bookings/:id/edit" do
-  @booking = Booking.find(params[:id])
-  erb(:"bookings/edit")
-end
-
-post "/bookings/:id" do
-  Booking.new(params).update
-  redirect to "/bookings"
-end
+# get "/bookings/:id" do
+#   @booking = Booking.find(params[:id])
+#   erb(:"bookings/show")
+# end
+#
+# get "/bookings/:id/edit" do
+#   @booking = Booking.find(params[:id])
+#   erb(:"bookings/edit")
+# end
+#
+# post "/bookings/:id" do
+#   Booking.new(params).update
+#   redirect to "/bookings"
+# end
 
 # post "/bookings/:id/delete" do
 #   @booking = Booking.find(params[:id])
